8648350dd69412fa74b9bdcc7ef3549e8a225514
bahilo <sisi.bahilo@gmail.com>
2016-01-26 07:28:22 +0000

    bug in chrome

--- DagoWebPortfolio/Content/Scripts/scripts.js
+++ DagoWebPortfolio/Content/Scripts/scripts.js
@@ -17,51 +17,6 @@ $(document).ready(function () {
           $('body,html').animate({scrollTop:0},1000);
         })
 
-        /* smooth scrolling for nav sections 
-        $('#nav .navbar-nav li>a').click(function(){
-          var link = $(this).attr('href');
-          var posi = $(link).offset().top;
-          $('body,html').animate({scrollTop:posi},700);
-        });*/
-
-
-
-
-        $('.experiences-home .text-right').mouseover(function () {
-            $(this).animate({ 'opacity': 0.5 }, "slow");
-        });
-        $('.experiences-home .text-right').mouseout(function () {
-            $(this).animate({ 'opacity': 1 }, "slow");
-        });/**/
-
-        $('.experiences-home .text-left').mouseover(function () {
-            $(this).animate({ 'opacity': 0.5 }, "slow");
-        });
-        $('.experiences-home .text-left').mouseout(function () {
-            $(this).animate({ 'opacity': 1}, "slow");
-        });/**/
-
-
-        /*$('.panel').on('load', function () {
-
-        }).each(function (i) {
-            $(this).mouseenter(function () {
-                $(this).animate({ 'opacity': 0.5, 'width':'=-20px' });
-            });
-            $(this).mouseout(function () {
-                $(this).animate({ 'opacity': 1, 'width': '=+20px' });
-            });
-        });*/
-
-
-
-        /* activate the carousel */
-        //$('#modalCarousel').carousel({interval:false});
-
-        /* change modal title when slide changes */
-        /*$('#modalCarousel').on('slid.bs.carousel', function () {
-          $('.modal-title').html($(this).find('.active').attr("title"));
-        })*/
 
         /* when clicking a thumbnail */
         $('.panel-thumbnail>a').click(function(e){
@@ -75,11 +30,8 @@ $(document).ready(function () {
   	        return false;
         });
 
-        /*$("canvas[id^=demo]").each(function () {
-            $(this)
-        });*/
-
-
+       
+       /**/
       tinymce.init({
           selector: 'textarea',
           plugin: 'a_tinymce_plugin',
@@ -87,6 +39,6 @@ $(document).ready(function () {
           a_configuration_option: 400
       });
 
-
+   
 
 });
